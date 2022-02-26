f70f65d7b8e56bc7078b30e4a83b46e4299dcc8e
Fachreza Maulana <zzafachreza@gmail.com>
2022-02-26 08:54:27 +0700

    alhamdululillah

---
+++ src/utils/colors/index.js
@@ -0,0 +1,15 @@
+export const colors = {
+  primary: '#0B7CBE',
+  secondary: '#FD7400',
+  tertiary: '#1AC3BE',
+  background: '#6B63FF',
+  success: '#198754',
+  warning: '#F37120',
+  danger: '#dc3545',
+  black: 'black',
+  white: 'white',
+  border: '#7D8797',
+  zavalabs: '#1A3A59',
+  zavalabs2: '#FB6D39'
+};
+
